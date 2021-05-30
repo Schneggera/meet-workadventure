36c532dffe93acfbfa7ff6c899e43277cc151935
Tobias <tobias@re1chel.de>
2021-05-30 14:14:01 +0200

    changed tiles

--- cija.tsx
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.6.0" name="cija" tilewidth="32" tileheight="32" tilecount="2394" columns="18">
- <image source="wa-map-fachschaft-main-tilesets/tilesets/cija_32x32_mod.png" width="576" height="4256"/>
- <tile id="558">
-  <properties>
-   <property name="collides" type="bool" value="true"/>
-  </properties>
- </tile>
-</tileset>
